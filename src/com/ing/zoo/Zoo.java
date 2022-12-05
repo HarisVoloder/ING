33f67a8feaed49dae0ea10d1be3a3c9a3d12f70f
Haris Voloder <haris.voloder@hva.nl>
2022-12-05 12:33:48 +0100

    Test

--- src/com/ing/zoo/Zoo.java
+++ src/com/ing/zoo/Zoo.java
@@ -10,7 +10,7 @@ public class Zoo {
         commands[1] = "give leaves";
         commands[2] = "give meat";
         commands[3] = "perform trick";
-        System.out.println("Test");
+        System.out.println("Dev");
 
         Lion henk = new Lion();
         henk.name = "henk";
